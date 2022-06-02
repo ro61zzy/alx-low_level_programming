27a00e237f3638c745c31b3b1afbdbda7aaa482e
rose <rosekaremeri@gmail.com>
2022-06-02 13:33:49 -0700

    prints a quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - main function
- *
+ ** Description: 'and that piece of art is useful" - Dora korpar, 2015-10-19'
  * Return: 1
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
