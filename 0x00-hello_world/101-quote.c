84917dc9fb7fbfcd96b10d345ccd7d6391666454
rose <rosekaremeri@gmail.com>
2022-06-02 13:27:28 -0700

    prints messege

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 1
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+return (1);
+}
