b5c9064198bbfeeebee67b6b913c2a6e488cd767
rose <rosekaremeri@gmail.com>
2022-06-03 06:09:50 -0700

    single of base 10 with a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+putchar(n + '0');
+putchar('\n');
+return (0);
+}
