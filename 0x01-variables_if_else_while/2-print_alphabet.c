9b4a6050ff72e81cf1e540496b38647114bcfb70
rose <rosekaremeri@gmail.com>
2022-06-03 03:46:52 -0700

    small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwyas 0 (Success)
+ */
+int main(void)
+{
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
