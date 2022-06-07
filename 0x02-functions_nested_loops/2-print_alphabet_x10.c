dae52c46083178ab1517a363a780cfde35ff18ab
rose <rosekaremeri@gmail.com>
2022-06-07 13:50:38 -0700

    prints all alphabets x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabet 10 times
+ *Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int i;
+i = 0;
+
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
+}
