32d89e06104e16aa46b7639c727de87c5870d3ea
rose <rosekaremeri@gmail.com>
2022-06-07 16:42:16 -0700

    prints number from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
- * @: print from this number
+ * @n: print from this number
  */
 void print_to_98(int n)
 {
