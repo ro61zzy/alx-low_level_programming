30fd69b88d2d1384f6e6c63fa5d1090130860564
rose <rosekaremeri@gmail.com>
2022-06-08 11:52:21 -0700

    test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,31 @@
 #include "main.h"
-void positive_or_negative(int i);
+
 /**
- * main - main
  *
- * Return: 0
- */
+ *  * main - main
+ *
+ *   * Return: 0
+ *
+ *    */
+
+
+
 int main(void)
+
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+
+	int i;
+
+	i = 0;
+
+
+
+	positive_or_negative(i);
+
+
+
+	return (0);
+
+
+
 }
