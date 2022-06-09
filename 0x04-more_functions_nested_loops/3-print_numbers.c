28569ba6fb395a61142df5c187f13bf845977963
rose <rosekaremeri@gmail.com>
2022-06-09 12:04:59 -0700

    prints numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * print_numbers - prints numbers 0 to 9, with a new line
+  * Return: void
+  */
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
